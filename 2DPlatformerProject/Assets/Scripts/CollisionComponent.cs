757f20bfa9af4e51c132d525728f15e42a97c27f
GLGD_Kong <qls8920@gmail.com>
2024-11-03 14:56:46 +0900

    2ㅇ
    포트폴리오 파일 옮김

--- 2DPlatformerProject/Assets/Scripts/CollisionComponent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CollisionComponent : MonoBehaviour
-{
-    protected HealthControlComponent hp;
-
-}
